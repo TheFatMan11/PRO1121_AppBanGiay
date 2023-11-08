32cafabbbac7ee894c7e4c427f57427b36eae064
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-11-08 12:29:02 +0700

    08/11/2023

---
+++ app/src/main/java/com/thuydev/pro1121_appbangiay/ManHinhChao_Activity.java
@@ -0,0 +1,53 @@
+package com.thuydev.pro1121_appbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ManHinhChao_Activity extends AppCompatActivity {
+    SharedPreferences preferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+        preferences = getSharedPreferences("begin",MODE_PRIVATE);
+        int i =  preferences.getInt("only",0);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i==0){
+                    Intent intent = new Intent(ManHinhChao_Activity.this,ManHinhKhoiDau_Activity.class);
+                    startActivity(intent);
+                    thaygiatri();
+                }else {
+                dangNhap();
+                }
+                finish();
+            }
+        },3000);
+    }
+
+    private void thaygiatri() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("only",1);
+        editor.apply();
+    }
+
+    private void dangNhap() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Intent intent;
+        if (user==null){
+            Toast.makeText(this, "Chưa có tài khoản", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Vào màn hình đăng nhập", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
