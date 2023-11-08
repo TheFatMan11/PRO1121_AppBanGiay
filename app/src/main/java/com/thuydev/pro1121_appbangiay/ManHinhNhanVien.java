7c6dde2518308dd15ea6b0b7a07053474aad4cb5
CuongDang <cuongdang5124@gmail.com>
2023-11-09 00:10:12 +0700

    Cuong 8/11/2023

---
+++ app/src/main/java/com/thuydev/pro1121_appbangiay/ManHinhNhanVien.java
@@ -0,0 +1,14 @@
+package com.thuydev.pro1121_appbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManHinhNhanVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_nhan_vien);
+    }
+}
