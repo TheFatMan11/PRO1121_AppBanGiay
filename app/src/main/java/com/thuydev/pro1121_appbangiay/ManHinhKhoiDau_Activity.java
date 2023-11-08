f9bbc257019a4f2da6da6c5864be343dadd1d2b3
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-11-08 12:29:02 +0700

    08/11/2023

---
+++ app/src/main/java/com/thuydev/pro1121_appbangiay/ManHinhKhoiDau_Activity.java
@@ -0,0 +1,18 @@
+package com.thuydev.pro1121_appbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ManHinhKhoiDau_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_khoi_dau);
+
+    }
+}
