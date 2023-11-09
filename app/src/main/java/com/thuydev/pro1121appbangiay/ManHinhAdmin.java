1da7815a4e5f530b5cc39417a8e8fd8351f8a03f
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-11-09 14:34:20 +0700

    thuy 09/11/2023

--- app/src/main/java/com/thuydev/pro1121appbangiay/ManHinhAdmin.java
+++ app/src/main/java/com/thuydev/pro1121appbangiay/ManHinhAdmin.java
@@ -3,6 +3,7 @@ package com.thuydev.pro1121appbangiay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
@@ -12,7 +13,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.thuydev.pro1121appbangiay.adapter.viewPagerAdapter;
@@ -39,6 +40,34 @@ public class ManHinhAdmin extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_admin);
         viewPager = findViewById(R.id.fcv_Admin);
         bottomNavigationView = findViewById(R.id.bnv_Admin);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Quản Lý Nhân Viên");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_admin_qlkh){
+                    Toast.makeText(ManHinhAdmin.this, "Đang ở quản lý khách hàng", Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Quản Lý Khách Hàng");
+                }else if (item.getItemId() == R.id.menu_admin_qlnv){
+                    Toast.makeText(ManHinhAdmin.this, "Đang ở quản lý nhân viên", Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Quản Lý Nhân Viên");
+                }else if (item.getItemId() == R.id.menu_admin_qlsp){
+                    Toast.makeText(ManHinhAdmin.this, "Đang ở quản lý sản phẩm", Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Quản Lý Sản Phẩm");
+                }else if (item.getItemId() == R.id.menu_admin_thongke){
+                    Toast.makeText(ManHinhAdmin.this, "Đang ở thống kê", Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Thống kê");
+                } else if (item.getItemId()== R.id.menu_admin_resetpass) {
+                    Toast.makeText(ManHinhAdmin.this, "Đang ở đổi mật khẩu", Toast.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle("Đổi mật khẩu");
+                }else {
+                    Toast.makeText(ManHinhAdmin.this, "Lỗi", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
 
 
     }
