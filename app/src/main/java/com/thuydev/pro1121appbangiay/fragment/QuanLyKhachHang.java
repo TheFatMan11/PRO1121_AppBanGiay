6836971bf07bfe9a72bd585b739339df8f698571
Minh <chamvaonoidau1218@gmail.com>
2023-11-12 19:25:31 +0700

    Cuong 12/11/2023

--- app/src/main/java/com/thuydev/pro1121appbangiay/fragment/QuanLyKhachHang.java
+++ app/src/main/java/com/thuydev/pro1121appbangiay/fragment/QuanLyKhachHang.java
@@ -1,66 +1,130 @@
 package com.thuydev.pro1121appbangiay.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.thuydev.pro1121appbangiay.R;
+import com.thuydev.pro1121appbangiay.adapter.AdapterUser;
+import com.thuydev.pro1121appbangiay.model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QuanLyKhachHang#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
 public class QuanLyKhachHang extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    List<User> list ;
+    EditText edt_maNV, edt_Email, edt_matKhau, edt_hoTen, edt_sdt, edt_cv;
+    AppCompatButton btn_Luu, btn_Huy;
+    RecyclerView recyclerView;
+    ImageButton button;
+    AdapterUser adapterUser;
+    String id;
+    User user = new User();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth firebaseAuth;
+    String email,matkhau,hoten,sdt,cv;
+    Dialog dialog;
     public QuanLyKhachHang() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuanLyKhachHang.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuanLyKhachHang newInstance(String param1, String param2) {
-        QuanLyKhachHang fragment = new QuanLyKhachHang();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quan_ly_khach_hang, container, false);
+        View view = inflater.inflate(R.layout.fragment_quan_ly_khach_hang, null);
+        recyclerView = view.findViewById(R.id.rcv_qlkh);
+        loatData();
+        firebaseAuth = FirebaseAuth.getInstance();
+        return view;
     }
+
+    public boolean isValidateEmail(CharSequence e) {
+        return !TextUtils.isEmpty(e) && Patterns.EMAIL_ADDRESS.matcher(e).matches();
+    }
+
+    public boolean isValidatePhone(CharSequence e) {
+        return !TextUtils.isEmpty(e) && Patterns.PHONE.matcher(e).matches();
+    }
+
+    public void loatData() {
+        nghe();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list =new ArrayList<>();
+        recyclerView.setAdapter(new AdapterUser(getContext(), list));
+        adapterUser = new AdapterUser(getContext(), list);
+        recyclerView.setAdapter(adapterUser);
+    }
+
+
+
+
+    private void nghe() {
+        db.collection("user").whereEqualTo("chucVu", 3).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    return;
+                }
+                if (value != null) {
+                    for (DocumentChange dc : value.getDocumentChanges()) {
+                        Log.e("TAG", "onEvent: "+ dc.getType() );
+                        switch (dc.getType()) {
+                            case ADDED:
+                                dc.getDocument().toObject(User.class);
+                                list.add(dc.getDocument().toObject(User.class));
+                                adapterUser.notifyDataSetChanged();
+                                break;
+                            case MODIFIED:
+                                User user1 = dc.getDocument().toObject(User.class);
+                                if (dc.getOldIndex() == dc.getNewIndex()) {
+                                    list.set(dc.getOldIndex(), user1);
+                                    adapterUser.notifyDataSetChanged();
+                                } else {
+                                    list.remove(dc.getOldIndex());
+                                    list.add(user1);
+                                    adapterUser.notifyDataSetChanged();
+                                }
+                                break;
+                            case REMOVED:
+                                dc.getDocument().toObject(User.class);
+                                list.remove(dc.getOldIndex());
+                                adapterUser.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
 }
