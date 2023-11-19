2a1b1c6d857a7bd1bd3ad44e4d1b5d74b0919e74
Minh <chamvaonoidau1218@gmail.com>
2023-11-19 11:11:43 +0700

    minh 19/11

--- app/src/main/java/com/thuydev/pro1121appbangiay/fragment/ThongKe_DoanhThu.java
+++ app/src/main/java/com/thuydev/pro1121appbangiay/fragment/ThongKe_DoanhThu.java
@@ -44,7 +44,7 @@ public class ThongKe_DoanhThu extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_thong_ke__doanh_thu, null)
+        View view = inflater.inflate(R.layout.fragment_thong_ke__doanh_thu, null);
         edt_NgayEnd = view.findViewById(R.id.edt_ngayEnd);
         edt_NgayStart = view.findViewById(R.id.edt_ngayStart);
         tv_chonNgayStart = view.findViewById(R.id.tv_chonNgayStart);
